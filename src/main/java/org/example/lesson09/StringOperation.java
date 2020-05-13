1fbb16ebb4f5dfc16df941cdadbff991317d1b18
BABA <jaromarg@gmail.com>
2020-05-13 21:25:57 +0200

    halfSum

--- src/main/java/org/example/lesson09/StringOperation.java
+++ src/main/java/org/example/lesson09/StringOperation.java
@@ -12,15 +12,38 @@ public class StringOperation {
             for (int i = 0; i < str.length(); i++) {
                 result = str.charAt(i) == mySimple.charAt(i + index);
             }
-        }else {
+        } else {
             result = true;
         }
         return result;
     }
-    public static String convertDate(String str){
+
+    public static String convertDate(String str) {
         String[] words = str.split("-");
         String result;
         result = words[1] + "/" + words[2] + "/" + words[0];
+        return result;
+    }
+
+    public static String checkHalfSum(String str) {
+        String result = "";
+        int prevSum = 0;
+        int tmpSum = 0;
+        int stringLengthHalf = str.length() / 2;
+        String[] parts = {str.substring(0, stringLengthHalf), str.substring(stringLengthHalf)};
+        for (String part : parts) {
+            for (char c : part.toCharArray()) {
+                // example decimal value of character 5 is 53 and 0 is 48. So 53 - 48 = 5
+                tmpSum += c - '0';
+            }
+            if (tmpSum == prevSum) {
+                result = "YES";
+            } else {
+                prevSum = tmpSum;
+                tmpSum = 0;
+                result = "NO";
+            }
+        }
         return result;
     }
 }
--- src/test/java/org/example/lesson09/StringOperationTest.java
+++ src/test/java/org/example/lesson09/StringOperationTest.java
@@ -24,4 +24,13 @@ class StringOperationTest {
     void convertDate() {
         assertEquals("09/28/2012", StringOperation.convertDate("2012-09-28"));
     }
+
+    @Test
+    void checkHalfSumPositive() {
+        assertEquals("YES", StringOperation.checkHalfSum("12344321"));
+    }
+    @Test
+    void checkHalfSumNegative() {
+        assertEquals("NO", StringOperation.checkHalfSum("125322"));
+    }
 }
