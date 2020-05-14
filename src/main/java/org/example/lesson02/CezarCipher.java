30ae4f3391396aec6ec1f5f6c766b24bde55cc0a
BABA <jaromarg@gmail.com>
2020-05-14 21:17:41 +0200

    shiftEncrypt

--- src/main/java/org/example/lesson02/CezarCipher.java
+++ src/main/java/org/example/lesson02/CezarCipher.java
@@ -22,5 +22,29 @@ public class CezarCipher {
         }
         return result;
     }
+
+    public static String encryptShift(String text, int shift) {
+        // String is immutable whereas StringBuffer and StringBuider are mutable classes.
+        // StringBuffer is thread safe and synchronized whereas StringBuilder is not,
+        // that is why StringBuilder is more faster than StringBuffer.
+        StringBuilder result = new StringBuilder();
+
+        String mySimple = "abcdefghijklmnopqrstuvwxyz";
+
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isLetter(text.charAt(i))) {
+                int idx = mySimple.indexOf(text.charAt(i));
+                int newIdx = idx + shift;
+                if (newIdx > 25) {
+                    newIdx -= 26;
+                }
+                char hash = mySimple.charAt(newIdx);
+                result.append(hash);
+            } else {
+                result.append(text.charAt(i));
+            }
+        }
+        return result.toString();
+    }
 }
 
