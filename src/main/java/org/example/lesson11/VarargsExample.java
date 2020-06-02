910027b17f9cd5e3203975e2c6cbf79217d9a1bd
BABA <jaromarg@gmail.com>
2020-06-02 20:43:30 +0200

    VarargsExample

--- src/main/java/org/example/lesson11/VarargsExample.java
+++ src/main/java/org/example/lesson11/VarargsExample.java
@@ -9,8 +9,16 @@ public class VarargsExample {
         printNumberOfArguments(1);
         printNumberOfArguments(1,2,3);
         printNumberOfArguments(new int[] { 1, 2 });
+
     }
 
     // This example also demonstrates the difference between the arguments and parameters of a method.
     // The method has only a single parameter but it can be called with several arguments.
+
+    // If a method has more than one parameter,
+    // a vararg parameter must be the last parameter in the declaration of the method.
+
+    // wrong is:
+    // public static void printNumberOfArgumentsWrong(int... numbers, double x) {
+    // }
 }
