42878db40b8cd9df4cfc56553a8d42545d052c53
BABA <jaromarg@gmail.com>
2020-06-08 21:12:18 +0200

    str

---
+++ src/main/java/org/example/lesson12/StringOptymalization.java
@@ -0,0 +1,35 @@
+package org.example.lesson12;
+
+import java.sql.Time;
+
+public class StringOptymalization {
+    public static void main(String[] args){
+
+        String s = "a";
+
+        long startOne = System.nanoTime();
+        for(int i=0; i<10000; i++) {
+            s = s + "a";
+        }
+        System.out.println("Time1: "+(System.nanoTime()-startOne));
+        System.out.println(s);
+
+        // Time1: 80 595 300
+
+        String s2 = "b";
+
+        long startTwo = System.nanoTime();
+        StringBuilder sB = new StringBuilder(s2);
+        for(int i=0; i<10000; i++) {
+            sB.append("b");
+        }
+
+        s2 = sB.toString();
+        System.out.println("Time2: "+(System.nanoTime()-startTwo));
+        System.out.println(s2);
+
+        //Time2: 2 327 700
+
+
+    }
+}
